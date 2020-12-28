746da14095857853dc346bcecec4b2a35e26db80
aden <kr.one2ten@gmail.com>
2020-12-28 09:55:31 +0900

    =debugging for colab

--- appearance_generation/models/ov_pix2pixHD_model.py
+++ appearance_generation/models/ov_pix2pixHD_model.py
@@ -164,7 +164,7 @@ class Pix2PixHDModel(BaseModel):
         app_feature_vec_temp = y.clone()
         for num_seg_channel in range(20):
             
-            print("num_seg_channel : {} [{}] ".format(num_seg_channel,num_seg_channel.shape))
+            print("num_seg_channel : {} ".format(num_seg_channel))
             print("selected_seg_mask_tensor : {} [{}] ".format(selected_seg_mask_tensor,selected_seg_mask_tensor.shape))           
             
             print(int(selected_seg_mask_tensor))
