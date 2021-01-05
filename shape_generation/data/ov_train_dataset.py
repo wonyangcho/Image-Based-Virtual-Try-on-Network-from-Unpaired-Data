7c6179ec2ce31f45003805b1532fefc951fc0d73
wonyangcho <kr.one2ten@gmail.com>
2021-01-05 15:33:26 +0900

    =fix

--- shape_generation/data/ov_train_dataset.py
+++ shape_generation/data/ov_train_dataset.py
@@ -79,90 +79,30 @@ class RegularDataset(Dataset):
         B_path = self.B_paths[index]
         B = self.parsing_embedding(B_path, 'seg')  # channel(20), H, W
 
-
         # densepose maps
-        #  새 모듈 시작
-        #print("denspose_paths : {}".format(self.densepose_paths[index]))
+        dense_path = self.densepose_paths[index]
+        dense_img = np.load(dense_path).astype('uint8')
+        dense_img_parts_embeddings = self.parsing_embedding(
+            dense_img[:, :, 0], 'densemap')
 
-        # |labels| = [H,W]
-        # |uv| = [2,H,W]
-
-       
+        dense_img_parts_embeddings = np.transpose(
+            dense_img_parts_embeddings, axes=(1, 2, 0))
+        dense_img_final = np.concatenate(
+            (dense_img_parts_embeddings, dense_img[:, :, 1:]), axis=-1)  # channel(27), H, W
 
         # original seg mask
-        with Image.open(A_path) as seg_mask :
-       
-            #seg_mask = seg_mask.resize((self.img_width,self.img_height),Image.BICUBIC)  #이미지 해상도를  미리 resize 해두자.
-            seg_mask = np.array(seg_mask)
-            seg_mask = torch.tensor(seg_mask, dtype=torch.long)
+        seg_mask = Image.open(A_path)
+        seg_mask = np.array(seg_mask)
+        seg_mask = torch.tensor(seg_mask, dtype=torch.long)
 
         # final returns
         A_tensor = self.custom_transform(A, True)
         B_tensor = torch.from_numpy(B)
+        dense_img_final = torch.from_numpy(
+            np.transpose(dense_img_final, axes=(2, 0, 1)))
 
-        dense_path = self.densepose_paths[index]
-
-        
-        dense_img = np.load(dense_path).astype('uint8')
-        print("dense_img shape : {}".format(dense_img))
-        
-
-        dense_img_parts_embeddings = self.parsing_embedding(dense_img[:, :, 0], 'densemap')
-
-        print("dense_img_parts_embeddings 1 shape {}".format(dense_img_parts_embeddings.shape))
-
-        dense_img_parts_embeddings = np.transpose(dense_img_parts_embeddings, axes=(1, 2, 0))
-        print("dense_img_parts_embeddings 2 shape {}".format(dense_img_parts_embeddings.shape))
-
-        dense_img_final = np.concatenate((dense_img_parts_embeddings, dense_img[:, :, 1:]), axis=-1)  # channel(27), H, W
-        print("dense_img_final shape {}".format(dense_img_final.shape))
-
-        # with open(dense_path, 'rb') as f:
-        #     densepose_pkl_data = pickle.load(f)
-        #     pred_densepose = densepose_pkl_data[0]['pred_densepose']
-
-        #     #print("densepose_pkl_data : {}".format(densepose_pkl_data[0]))
-        #     #print("pred dense boxes : {} ({} )".format(densepose_pkl_data[0]['pred_boxes_XYXY'],densepose_pkl_data[0]['pred_boxes_XYXY'].shape))
-        #     #print("pred dense pose label : {} ( {} )".format(pred_densepose[0].labels,pred_densepose[0].labels.shape))
-
-        #     bbox_xywh = densepose_pkl_data[0]['pred_boxes_XYXY'][0]
-        #     x, y, w, h = int(bbox_xywh[0]), int(bbox_xywh[1]), int(bbox_xywh[2]-bbox_xywh[0]), int(bbox_xywh[3]-bbox_xywh[1])
-        #     #print("pred dense boxes : {} {} {} {}".format(x,y,w,h))
-
-        #     # org_file_path = os.path.basename(densepose_pkl_data[0]['file_name'])
-        #     # org_file_path = os.path.join(self.opt.dataroot, self.opt.phase, org_file_path)
-        #     # #print("orginal file path : {}".format(org_file_path))
-
-
-        #     # temp_w,temp_h = Image.open(org_file_path).size
-        #     # #print("orginal file size : [{}, {} ]".format(temp_h,temp_w))
-
-        #     img_final_arr =  np.zeros((self.img_height,self.img_width,3))
-            
-            
-
-        #     iuv_arr = torch.cat([pred_densepose[0].labels.unsqueeze(0), pred_densepose[0].uv],0).cpu().numpy()
-        #     #print("pred iuv_arr shape : {}".format(iuv_arr.shape))
-            
-        #     mask = np.transpose(iuv_arr,(1,2,0))
-        #     img_final_arr[y:y+h,x:x+w,:] = mask
-        #     dense_img = img_final_arr.astype(np.uint8)
-            
-        #     dense_img = Image.fromarray(dense_img.astype(np.uint8))
-        #     dense_img = dense_img.resize((self.img_width,self.img_height),Image.BICUBIC)
-        #     dense_img = np.array(dense_img)
-
-        #     dense_img_parts_embeddings = self.parsing_embedding(dense_img[:, :, 0], 'densemap')
-
-        #     dense_img_parts_embeddings = np.transpose(dense_img_parts_embeddings, axes=(1, 2, 0))
-        #     dense_img_final = np.concatenate((dense_img_parts_embeddings, dense_img[:, :, 1:]), axis=-1)  # channel(27), H, W
-        #     #print("dense_img_final shape {} ".format(dense_img_final.shape))
-        
-        # dense_img_final = torch.from_numpy(np.transpose(dense_img_final, axes=(2, 0, 1)))
-        
-
-        input_dict = {'seg_map': A_tensor, 'dense_map': dense_img_final, 'target': B_tensor, 'seg_map_path': A_path, 'target_path': A_path, 'densepose_path': dense_path, 'seg_mask': seg_mask}
-        
+        input_dict = {'seg_map': A_tensor, 'dense_map': dense_img_final, 'target': B_tensor, 'seg_map_path': A_path,
+                      'target_path': A_path, 'densepose_path': dense_path, 'seg_mask': seg_mask}
 
         return input_dict
 
@@ -170,13 +110,13 @@ class RegularDataset(Dataset):
         
         if parse_type == "seg":
             parse = Image.open(parse_obj)
-            #parse = parse.resize((self.img_width,self.img_height),Image.NEAREST)    #이미지 해상도를  미리 resize 해두자.    
+            parse = parse.resize((self.img_width,self.img_height),Image.NEAREST)    #이미지 해상도를  미리 resize 해두자.    
             parse = np.array(parse)
             parse_channel = 20
 
         elif parse_type == "densemap":
             parse = np.array(parse_obj)
-            parse_channel = 25
+            parse_channel = 27
 
         parse_emb = []
 
